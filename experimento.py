968441c37d9dcd8b6149551cdde107aa04cc8535
Mario augusto Frias <marioaugustofrias2001@gmail.com>
2026-06-23 09:37:53 -0300

    Corrigindo loop e adicionando atualização automática do ano
    Melhoria na validação e atualização automática do cálculo de IPVA

--- experimento.py
+++ experimento.py
@@ -1,21 +1,37 @@
+from datetime import datetime
+
 while True:
     print('\033[;31;40m-=-\033[m' * 19)
-    print('\033[7;31;40mConsulta de IPVA - erifique se seu carro ainda paga IPVA\033[m')
+    print('\033[7;31;40mConsulta de IPVA - Verifique se seu carro ainda paga IPVA\033[m')
     print('\033[;31;40m-=-\033[m' * 19)
 
-    ano = int(input('+Qual o ano do seu carro? '))
-    carro = 2025 - ano
-    anoc = 20 - carro
+    try:
+        ano = int(input('+ Qual o ano do seu carro? '))
 
-    if carro < 0:
-        print("🚨 Ano inválido! Digite um ano menor ou igual a 2025.")
-    elif carro <= 20:
-        print(f'Seu carro tem {carro} anos. Ainda faltam {anoc} anos para que ele fique isento de IPVA.')
-    else:
-        print(f'Seu carro tem {carro} anos. Ele já não paga mais IPVA, apenas o licenciamento!')
+        ano_atual = datetime.now().year
+        idade_carro = ano_atual - ano
+        anos_restantes = 20 - idade_carro
 
-    # Pergunta se quer repetir
+        if idade_carro < 0:
+            print("🚨 Ano inválido! Digite um ano menor ou igual ao ano atual.")
+
+        elif idade_carro < 20:
+            print(
+                f'Seu carro tem {idade_carro} anos. '
+                f'Ainda faltam {anos_restantes} anos para possível isenção de IPVA.'
+            )
+
+        else:
+            print(
+                f'Seu carro tem {idade_carro} anos. '
+                f'Ele já pode estar isento de IPVA (verifique as regras do seu estado).'
+            )
+
+    except ValueError:
+        print("🚨 Digite apenas números.")
+
     novac = input('\nGostaria de fazer uma nova consulta? (s/n): ').strip().lower()
+
     if novac not in ['s', 'sim']:
         print("\nObrigado por usar o sistema! 🚗💨")
-    break
+        break
